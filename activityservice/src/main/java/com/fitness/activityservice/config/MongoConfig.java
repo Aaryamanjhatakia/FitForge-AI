f10761a23eefe214a1733ab1ceada505c5360e8d
Aaryamanjhatakia <149300617+Aaryamanjhatakia@users.noreply.github.com>
2026-03-13 18:16:23 +0530

    Updated Activity Service and Added Eureka Server

--- activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
@@ -4,9 +4,11 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
+@EnableMongoAuditing
 public class MongoConfig {
 
     @Bean
