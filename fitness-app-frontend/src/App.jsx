9930cee5d9e437f3f739293c0c6f7565059f1e74
Aaryamanjhatakia <149300617+Aaryamanjhatakia@users.noreply.github.com>
2026-04-22 18:10:24 +0530

    Updated Frontend

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, Box } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "react-oauth2-code-pkce";
 import { useDispatch } from "react-redux";
@@ -10,6 +10,18 @@ import {
   useLocation,
 } from "react-router";
 import { setCredentials } from "./store/authSlice";
+import ActivityForm from "./components/ActivityForm";
+import ActivityList from "./components/ActivityList";
+import ActivityDetail from "./components/ActivityDetail";
+
+const ActivitiesPage = () => {
+  return (
+    <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+      <ActivityForm onActivitiesAdded={() => window.location.reload()} />
+      <ActivityList />
+    </Box>
+  );
+};
 
 function App() {
   const { token, tokenData, logIn, logOut, isAuthenticated } =
@@ -41,9 +53,27 @@ function App() {
           LOGIN{" "}
         </Button>
       ) : (
-        <div>
-          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
-        </div>
+        // <div>
+        //   <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+        // </div>
+
+        <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+          <Routes>
+            <Route path="/activities" element={<ActivitiesPage />} />
+            <Route path="/activities/:id" element={<ActivityDetail />} />
+
+            <Route
+              path="/"
+              element={
+                token ? (
+                  <Navigate to="/activities" replace />
+                ) : (
+                  <div>Welcome! Please login.</div>
+                )
+              }
+            />
+          </Routes>
+        </Box>
       )}
     </Router>
   );
